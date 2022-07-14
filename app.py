6e67c4b7c657dca7bcdbc8350bc9b012df77c42a
RodrigoCTdev <109238863+RodrigoCTdev@users.noreply.github.com>
2022-07-14 00:14:58 -0300

    definições de variavel numerica

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 a = 6
 b = 7 
 c = 8
+d = (a+b-c)
+print(d)
 
 print(a+c)
 print(b+c)
 print(a+b)
+
+if a < c:
+    print('o valor de a é maior que de c')
+else:
+    print('o valor de a é menor que o de c')
